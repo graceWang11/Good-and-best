5c9449023c74540f011c50797663d3d159395a92
graceWang11 <105827915+graceWang11@users.noreply.github.com>
2024-08-13 00:21:20 +1000

    Fix the bugs of displaying hello world displaying to the main page

--- 021/021/src/app/layout.tsx
+++ 021/021/src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import { ConvexClientProvider } from "../../Providers/convex-client-provider";
+import { ConvexProvider, ConvexReactClient } from "convex/react";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +17,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          {children}
+        </ConvexClientProvider>
+        
+        </body>
     </html>
   );
 }
--- 021/021/src/app/page.tsx
+++ 021/021/src/app/page.tsx
@@ -1,8 +1,17 @@
+"use client";
 import Image from "next/image";
 import ClientComponent from "./Components/ClientComponent";
-
+import { useEffect } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
 
 export default function Home() {
+
+  // const storeUser = useMutation(api.user.store);
+  
+  // useEffect(() => {
+  //   storeUser({});
+  // });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <ClientComponent />
