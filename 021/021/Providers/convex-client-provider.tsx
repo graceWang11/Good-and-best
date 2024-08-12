baf5f06155a836b1e3097af4cbf2413b5314453a
graceWang11 <105827915+graceWang11@users.noreply.github.com>
2024-08-12 23:58:31 +1000

    Updated main page with hello world display

---
+++ 021/021/Providers/convex-client-provider.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {Loading} from '@/components/auth/Loading';
+import { ClerkProvider, useAuth } from '@clerk/nextjs';
+import { AuthLoading , Authenticated , ConvexReactClient , Unauthenticated } from 'convex/react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+import React, { Children, use } from 'react';
+
+interface ConvexClientProviderProps{
+    children: React.ReactNode;
+};
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+export const ConvexClientProvider = (
+    {
+        children
+    }:ConvexClientProviderProps)=>{
+        return (
+            <ClerkProvider>
+                <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+                    <AuthLoading>
+                        <Loading />
+                    </AuthLoading>
+                    <Authenticated>
+                        {children}
+                    </Authenticated>
+                    <Unauthenticated>
+                        {children}
+                    </Unauthenticated>
+                </ConvexProviderWithClerk>
+            </ClerkProvider>
+        )
+    }
+
+
