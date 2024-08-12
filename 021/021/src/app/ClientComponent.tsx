70436895b44d1591b583c14572990b0451545884
graceWang11 <105827915+graceWang11@users.noreply.github.com>
2024-08-12 23:24:15 +1000

    Create ClientComponent.tsx

---
+++ 021/021/src/app/ClientComponent.tsx
@@ -0,0 +1,16 @@
+
+"use client";
+
+import { useEffect } from "react";
+import { useMutation } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+export default function ClientComponent() {
+  const storeUser = useMutation(api.user.store);
+
+  useEffect(() => {
+    storeUser({});
+  });
+
+  return <div>Hello World!</div>;
+}
