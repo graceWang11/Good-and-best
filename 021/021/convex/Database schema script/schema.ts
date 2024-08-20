e1ee0a5b2dce1c53a40a3d22f2d420f19b5c8d71
graceWang11 <105827915+graceWang11@users.noreply.github.com>
2024-08-20 22:32:08 +1000

    add orderHistory table

--- 021/021/convex/Database schema script/schema.ts
+++ 021/021/convex/Database schema script/schema.ts
@@ -2,28 +2,35 @@ import { defineSchema , defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    //Define UserType table 
-    userTypes: defineTable({
-      UserType: v.string(), 
-    }),
-    
-    //Define User Table 
-    users: defineTable({
-      tokenIdentifier: v.string(),
-      email: v.string(),
-      userTypeID: v.id("userTypes"),  // Reference to the userType table
-      userName: v.string(),
-      address: v.string(),
-      phoneNumber: v.string(),
-    })
-    .index("by_token", ["tokenIdentifier"]),
+  //Define UserType table 
+  userTypes: defineTable({
+    UserType: v.string(), 
+  }),
+  
+  //Define User Table 
+  users: defineTable({
+    tokenIdentifier: v.string(),
+    email: v.string(),
+    userTypeID: v.id("userTypes"),  // Reference to the userType table
+    userName: v.string(),
+    address: v.string(),
+    phoneNumber: v.string(),
+  })
+  .index("by_token", ["tokenIdentifier"]),
 
-    // Define the orders table
-    orders: defineTable({
-      userID: v.id("users"),   // Foreign key reference to users table
-      orderDate: v.string(),   // Store date as a string in ISO format
-      totalAmount: v.number(),
-      status: v.string(),
-    })
-    .index("by_user", ["userID"]),  // Index for querying by userID
-  });
+  // Define the orders table
+  orders: defineTable({
+    userID: v.id("users"),   // Foreign key reference to users table
+    orderDate: v.string(),   // Store date as a string in ISO format
+    totalAmount: v.number(),
+    status: v.string(),
+  })
+  .index("by_user", ["userID"]),  // Index for querying by userID
+
+  // Define the order history table
+  orderHistory: defineTable({
+    orderID: v.id("orders"),   // Foreign key reference to orders table
+    eventsDate: v.string(),    // Store date as a string in ISO format
+  })
+  .index("by_order", ["orderID"]),  // Index for querying by orderID
+});
