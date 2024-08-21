74daf443024b753ce2a1e03fd97b0e977200959f
graceWang11 <105827915+graceWang11@users.noreply.github.com>
2024-08-21 17:40:12 +1000

    moved user.ts and userType.ts out of the database schema script folder

--- 021/021/convex/_generated/api.d.ts
+++ 021/021/convex/_generated/api.d.ts
@@ -15,6 +15,8 @@ import type {
   FunctionReference,
 } from "convex/server";
 import type * as tasks from "../tasks.js";
+import type * as user from "../user.js";
+import type * as UserType from "../UserType.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -26,6 +28,8 @@ import type * as tasks from "../tasks.js";
  */
 declare const fullApi: ApiFromModules<{
   tasks: typeof tasks;
+  user: typeof user;
+  UserType: typeof UserType;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
--- 021/021/src/app/Components/ClientComponent.tsx
+++ 021/021/src/app/Components/ClientComponent.tsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { useMutation } from "convex/react";
 import { api } from "../../../convex/_generated/api";
 
+
 export default function ClientComponent() {
   const storeUser = useMutation(api.user.store);
 
