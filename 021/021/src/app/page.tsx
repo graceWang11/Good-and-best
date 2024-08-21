bbabc13a24f8aa953b944a387d7e10b75b35d50d
Yixin He <heyyy015@mymail.unisa.edu.au>
2024-08-21 22:50:25 +0930

    Update ClientComponent.tsx

--- 021/021/src/app/page.tsx
+++ 021/021/src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import Image from "next/image";
 import ClientComponent from "./Components/ClientComponent";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main >
       <ClientComponent />
     </main>
   );
