38ba6c53fbd148342dc4ae49d76a1185683efcec
graceWang11 <105827915+graceWang11@users.noreply.github.com>
2024-09-03 01:11:15 +1000

    generated size file for the raw data size table

--- 021/021/convex/_generated/api.d.ts
+++ 021/021/convex/_generated/api.d.ts
@@ -18,6 +18,7 @@ import type * as imageStorage from "../imageStorage.js";
 import type * as Product from "../Product.js";
 import type * as productAttributes from "../productAttributes.js";
 import type * as ProductCategory from "../ProductCategory.js";
+import type * as size from "../size.js";
 import type * as stock from "../stock.js";
 import type * as tasks from "../tasks.js";
 import type * as user from "../user.js";
@@ -36,6 +37,7 @@ declare const fullApi: ApiFromModules<{
   Product: typeof Product;
   productAttributes: typeof productAttributes;
   ProductCategory: typeof ProductCategory;
+  size: typeof size;
   stock: typeof stock;
   tasks: typeof tasks;
   user: typeof user;
