22ed4997395f8ab4112ada7aacaf3b8c1b4a970e
graceWang11 <105827915+graceWang11@users.noreply.github.com>
2024-11-03 22:37:56 +1100

    updated accessory page.tsx for lasted required pageprops

--- 021/021/src/app/Accessories/[...accessory]/page.tsx
+++ 021/021/src/app/Accessories/[...accessory]/page.tsx
@@ -1,8 +1,10 @@
+// Import necessary components
 import AccessoriesDetail from "@/app/Components/Accessories/AccessoriesDetail";
 import Footer from "@/app/Components/Footer";
 import PageNotFound from "@/app/Components/PageNotFound";
 import TopNavBar from "@/app/Components/TopNavBar";
 
+// Define the interface for the component props
 interface AccessoriesProps {
   params: {
     accessory: string[];
@@ -10,11 +12,12 @@ interface AccessoriesProps {
   searchParams: { [key: string]: string | string[] | undefined };
 }
 
-export default async function Accessories({ 
+// Define the Accessories component
+export default function Accessories({
   params,
   searchParams,
 }: AccessoriesProps) {
-  // Safely access and trim the first element
+  // Check if the accessory parameter has exactly one item
   if (params.accessory.length === 1) {
     const accessoryId = params.accessory[0];
     return (
@@ -25,6 +28,7 @@ export default async function Accessories({
       </>
     );
   } else {
+    // Render the PageNotFound component if the condition is not met
     return <PageNotFound />;
   }
 }
